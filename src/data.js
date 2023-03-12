d996167620f6151dc8492544976bb93ad068b5d5
Juantre82 <Juantre82@gmail.com>
2023-03-11 23:23:29 -0600

    making a new portfolio :)

---
+++ src/data.js
@@ -0,0 +1,47 @@
+export const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project1.png",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project2.png",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project3.png",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project4.png",
+      link: "https://reactbootcamp.com",
+    },
+  ];
+  
+  
+  export const skills = [
+    "JavaScript",
+    "React",
+    "HTML",
+    "CSS",
+    "Node",
+    "GraphQL",
+    "Express",
+    "MongoDB",
+    'AWS'
+  ];
