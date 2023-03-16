bb59ded46057ed45a9554779ff433a5f4c575261
Juantre82 <Juantre82@gmail.com>
2023-03-15 19:39:36 -0500

    updates to projects

--- src/data.js
+++ src/data.js
@@ -1,35 +1,34 @@
 export const projects = [
     {
-      title: "React Reserve",
-      subtitle: "MERN Stack",
+      title: "Tic Tac Toe",
+      subtitle: "React, HTML, CSS",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project1.png",
-      link: "https://reactbootcamp.com",
+      image: "./Tictactoe.gif",
+      link: "https://github.com/juantre82/tic-tac-toe",
     },
     {
-      title: "React Tracks",
-      subtitle: "React and Python",
+      title: "Eye Tracking App",
+      subtitle: "HTML, CSS, Javascript",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project2.png",
-      link: "https://reactbootcamp.com",
+      image: "./eyes.gif",
+      link: "https://juantre82.github.io/eye_project/",
     },
     {
-      title: "DevChat",
-      subtitle: "React and Firebase",
+      title: "Full Stack Bank App",
+      subtitle: "MERN STACK",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        "This project was made to showcase everything I have learned while attending MIT xPro and to encapslate the learnings into one project.",
       image: "./project3.png",
-      link: "https://reactbootcamp.com",
+      link: "https://github.com/juantre82/BadBankFullStack",
     },
     {
       title: "Epic Todo App",
       subtitle: "React Hooks",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project4.png",
-      link: "https://reactbootcamp.com",
+      description:'This app is a simple to do list app where you can add items to your list with the input field. You can also delete any items you want by clicking on it.',
+      image: "./Todo.gif",
+      link: "https://juantre82.github.io/ToDO/",
     },
   ];
   
@@ -40,8 +39,6 @@ export const projects = [
     "HTML",
     "CSS",
     "Node",
-    "GraphQL",
     "Express",
     "MongoDB",
-    'AWS'
   ];
